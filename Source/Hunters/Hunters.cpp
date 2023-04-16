df5c0e06d36432f85e60cf93001a18e43179231c
BigNinjaChicken <50557956+BigNinjaChicken@users.noreply.github.com>
2023-04-15 21:24:01 -0400

    Init

---
+++ Source/Hunters/Hunters.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Hunters.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Hunters, "Hunters" );
+ 
---
+++ Source/Hunters/HuntersGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HuntersGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHuntersGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHuntersGameMode();
+};
+
+
+
